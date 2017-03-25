ae4b3461e7ddb1ef7f84a79e4f92dc2889b60912
Narfss <narfss@gmail.com>
2017-03-25 15:02:39 +0100

    Final test

--- src/test/java/com/karumi/maxibonkata/KarumiHQProperty.java
+++ src/test/java/com/karumi/maxibonkata/KarumiHQProperty.java
@@ -3,17 +3,26 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import java.util.List;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 @RunWith(JUnitQuickcheck.class)
 public class KarumiHQProperty {
   private static final int MIN_MAXIBONS = 2;
-  private final KarumiHQs karumiHQs;
+  private KarumiHQs karumiHQs;
+  Chat chat;
 
-  public KarumiHQProperty() {
-    karumiHQs = new KarumiHQs();
+  @Before
+  public void before() {
+    chat = mock(Chat.class);
+    karumiHQs = new KarumiHQs(chat);
   }
 
   @Property
@@ -25,23 +34,39 @@ public class KarumiHQProperty {
 
   @Property
   public void minMaxibonbsLeftAfterHorde(List<@From(DevelopersGenerator.class) Developer> developers) {
-    //developers.forEach(System.out::println);
     karumiHQs.openFridge(developers);
 
     assertTrue(karumiHQs.getMaxibonsLeft() >= MIN_MAXIBONS);
   }
 
   @Property
-  public void minMaxibonbsLeftAfterHungryHorde(List<@From(HungryDevelopersGenerator.class) Developer> developers) {
-    //developers.forEach(System.out::println);
+  public void minMaxibonbsLeftAfterHungryChat(@From(HungryDevelopersGenerator.class) Developer developer) {
+    karumiHQs.openFridge(developer);
+
+    verify(chat).sendMessage(anyString());
+  }
+
+  @Property
+  public void minMaxibonbsLeftAfterHungryNoChat(@From(NotSoHungryDevelopersGenerator.class) Developer developer) {
+    karumiHQs.openFridge(developer);
+
+    verify(chat, never()).sendMessage(anyString());
+  }
+
+  @Property
+  public void minMaxibonbsLeftAfterHungryHordeChat(List<@From(HungryDevelopersGenerator.class) Developer> developers) {
+    developers.forEach(System.out::println);
     karumiHQs.openFridge(developers);
 
-    assertTrue(karumiHQs.getMaxibonsLeft() >= MIN_MAXIBONS);
+    if (developers.isEmpty()) {
+      verify(chat, never()).sendMessage(anyString());
+    } else {
+      verify(chat, atLeastOnce()).sendMessage(anyString());
+    }
   }
 
   @Property
   public void minMaxibonbsLeftAfterHordeGenerator(@From(ListDevelopersGenerator.class) DeveloperHorde developerHorde) {
-    //developers.forEach(System.out::println);
     karumiHQs.openFridge(developerHorde.getDevelopers());
 
     assertTrue(karumiHQs.getMaxibonsLeft() >= MIN_MAXIBONS);
